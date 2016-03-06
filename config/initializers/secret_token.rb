2d726532ad7d25a60cb4bc3387c427fb398af449
Kelly Gerritz <kelly.gerritz@tufts.edu>
2016-03-06 18:50:55 -0500

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Steamless::Application.config.secret_key_base = '5126360653a3159dbc7eb6843d133b74fb3c0e6fd25a1698002c518a201320dcea9e8c41e1b93f1e33a39730c0f2647f671a4a51f7e62e192da0a60d86a85834'
